bf9ec9db4770d90dd105c3485bb90826ae8ebd7d
sapoclay <admin@entreunosyceros.net>
2024-10-18 17:45:05 +0200

    Añadido el procesado de carpetas y el menú

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import flet as ft
 from rembg import remove
 from PIL import Image
+import pystray
+from pystray import MenuItem as Item, Icon
 import os
 import io
 import subprocess
 import platform
+from menu import Menu  # Importar el menú
+import threading
+
+# Obtén el directorio del archivo actual
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Función para eliminar el fondo de una imagen
 def eliminar_fondo(input_image_path, output_image_path):
@@ -16,20 +23,40 @@ def eliminar_fondo(input_image_path, output_image_path):
 
         img = Image.open(io.BytesIO(output_data)).convert("RGBA")
         img.save(output_image_path, format="PNG")
-        print(f"Imagen sin fondo guardada en {output_image_path}")
     except Exception as e:
         print(f"Error al eliminar el fondo: {e}")
 
-def eliminar_fondo_carpeta(carpeta_origen, carpeta_destino):
+def eliminar_fondo_carpeta(carpeta_origen, carpeta_destino, progress, progress_text, snack_bar, page):
     try:
-        for filename in os.listdir(carpeta_origen):
-            if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-                input_image_path = os.path.join(carpeta_origen, filename)
-                output_image_path = os.path.join(carpeta_destino, filename.replace(".png", "_sinfondo.png").replace(".jpg", "_sinfondo.png").replace(".jpeg", "_sinfondo.png"))
-                eliminar_fondo(input_image_path, output_image_path)
-        print("Fondo eliminado de todas las imágenes en la carpeta.")
+        imagenes = [f for f in os.listdir(carpeta_origen) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+        total = len(imagenes)
+        progress.value = 0
+        progress.visible = True  # Mostrar la barra de progreso al comenzar
+        progress_text.visible = True  # Mostrar el texto "Progreso:" al comenzar
+        page.update()
+
+        for index, filename in enumerate(imagenes, start=1):
+            input_image_path = os.path.join(carpeta_origen, filename)
+            output_image_path = os.path.join(carpeta_destino, filename.replace(".png", "_sinfondo.png").replace(".jpg", "_sinfondo.png").replace(".jpeg", "_sinfondo.png"))
+            eliminar_fondo(input_image_path, output_image_path)
+
+            # Actualiza el progreso
+            progress.value = index / total
+            snack_bar.content.value = f"Procesando {index}/{total}: {filename}"
+            snack_bar.open = True
+            page.update()
+
+        snack_bar.content.value = f"Fondo eliminado de todas las imágenes en {carpeta_origen}"
+        snack_bar.open = True
+        progress.visible = False  # Ocultar la barra de progreso al terminar
+        progress_text.visible = False  # Ocultar el texto "Progreso:" al terminar
+        page.update()
     except Exception as e:
-        print(f"Error al procesar la carpeta: {e}")
+        snack_bar.content.value = f"Error al procesar la carpeta: {e}"
+        snack_bar.open = True
+        progress.visible = False  # Ocultar la barra de progreso en caso de error
+        progress_text.visible = False  # Ocultar el texto "Progreso:" en caso de error
+        page.update()
 
 # Función para abrir la carpeta de destino
 def abrir_carpeta_destino(carpeta):
@@ -42,13 +69,36 @@ def abrir_carpeta_destino(carpeta):
             print("Sistema operativo no soportado")
     except Exception as e:
         print(f"Error al abrir la carpeta: {e}")
+        
+def create_tray_icon(page):
+    def quit_action(icon, item):
+        icon.stop()
 
-def main(page: ft.Page):
+
+    image = Image.open(os.path.join(BASE_DIR, "assets/tray-icon.png"))  # icono bandeja del sistema
+    icon = pystray.Icon("URL Shortener", image, menu=pystray.Menu(
+        Item('Mostrar ventana', lambda: None),  
+        Item('Salir', lambda icon, item: quit_action(icon, item))
+    ))
+    
+    def run_tray():
+        icon.run()
+
+    tray_thread = threading.Thread(target=run_tray, daemon=True)
+    tray_thread.start()
+
+async def main(page: ft.Page):
     page.title = "Quitar fondo de imágenes"
+    page.theme_mode = "dark"
+    
+    # Ajustar el tamaño de la ventana usando la nueva API
     page.window.width = 800
     page.window.height = 600
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    
+    # Inicializa el menú
+    Menu(page)  # Crear el menú en la ventana principal
 
     default_image_path = "assets/default-image.png"
 
@@ -57,6 +107,28 @@ def main(page: ft.Page):
     removed_bg_image_preview = ft.Image(src=default_image_path, width=150, height=150, fit=ft.ImageFit.CONTAIN)
     output_dir = ""
     input_image_path = ""
+    carpeta_origen = ""
+
+    # Añadir barra de progreso y texto de "Progreso", inicialmente invisibles
+    progress = ft.ProgressBar(width=400, visible=False)
+    progress_text = ft.Text("Progreso:", weight=ft.FontWeight.BOLD, visible=False)
+
+    # SnackBar para mostrar mensajes
+    snack_bar = ft.SnackBar(ft.Text(""))
+
+    # Función para restablecer los valores
+    def reset_app():
+        nonlocal input_image_path, output_dir, carpeta_origen
+        input_image_path = ""
+       # output_dir = ""
+        carpeta_origen = ""
+        img_input.value = "No hay imagen seleccionada"
+        original_image_preview.src = default_image_path
+        removed_bg_image_preview.src = default_image_path
+        progress.value = 0
+        progress.visible = False  # Ocultar la barra de progreso cuando se reinicie
+        progress_text.visible = False  # Ocultar el texto "Progreso" cuando se reinicie
+        page.update()
 
     # Función para manejar la selección de archivo (imagen)
     def on_file_chosen(e: ft.FilePickerResultEvent):
@@ -76,41 +148,42 @@ def main(page: ft.Page):
         nonlocal output_dir
         if e.path:
             output_dir = e.path
-            snack_bar = ft.SnackBar(ft.Text(f"Carpeta de destino seleccionada: {output_dir}"))
-            page.overlay.append(snack_bar)
+            snack_bar.content.value = f"Carpeta de destino seleccionada: {output_dir}"
             snack_bar.open = True
             page.update()
 
-            # Si hay una imagen cargada, procesarla
-            if input_image_path:
+            # Procesar todas las imágenes de la carpeta de origen
+            if carpeta_origen:
+                eliminar_fondo_carpeta(carpeta_origen, output_dir, progress, progress_text, snack_bar, page)
+                reset_app()  # Reiniciar la aplicación después de procesar
+            elif input_image_path:  # Procesar una sola imagen si no hay carpeta de origen
                 output_name = os.path.basename(input_image_path).rsplit('.', 1)[0] + "_sinfondo.png"
                 output_path = os.path.join(output_dir, output_name)
                 eliminar_fondo(input_image_path, output_path)
+                
+                # Asegúrate de que la imagen sin fondo se actualice en la vista previa
                 if os.path.exists(output_path):
                     removed_bg_image_preview.src = output_path
-                    snack_bar = ft.SnackBar(ft.Text(f"Imagen guardada en {output_path}"))
-                    page.overlay.append(snack_bar)
+                    snack_bar.content.value = f"Imagen guardada en {output_path}"
                     snack_bar.open = True
                 else:
                     removed_bg_image_preview.src = default_image_path
                 page.update()
+                reset_app()  # Reiniciar la aplicación después de procesar
 
     # Función para manejar la selección de carpeta con imágenes
     def on_folder_with_images_chosen(e: ft.FilePickerResultEvent):
+        nonlocal carpeta_origen
         if e.path:
             carpeta_origen = e.path
             if not output_dir:
-                snack_bar = ft.SnackBar(ft.Text("Por favor, selecciona una carpeta de destino antes de procesar las imágenes."))
-                page.overlay.append(snack_bar)
+                snack_bar.content.value = "Por favor, selecciona una carpeta de destino antes de procesar las imágenes."
                 snack_bar.open = True
                 page.update()
                 return
 
-            eliminar_fondo_carpeta(carpeta_origen, output_dir)
-            snack_bar = ft.SnackBar(ft.Text(f"Fondo eliminado de todas las imágenes en {carpeta_origen}"))
-            page.overlay.append(snack_bar)
-            snack_bar.open = True
-            page.update()
+            eliminar_fondo_carpeta(carpeta_origen, output_dir, progress, progress_text, snack_bar, page)
+            reset_app()  # Reiniciar la aplicación después de procesar
 
     file_picker = ft.FilePicker(on_result=on_file_chosen)
     page.overlay.append(file_picker)
@@ -138,8 +211,7 @@ def main(page: ft.Page):
         if output_dir:
             abrir_carpeta_destino(output_dir)
         else:
-            snack_bar = ft.SnackBar(ft.Text("Primero selecciona una carpeta de destino."))
-            page.overlay.append(snack_bar)
+            snack_bar.content.value = "Primero selecciona una carpeta de destino."
             snack_bar.open = True
             page.update()
 
@@ -159,39 +231,58 @@ def main(page: ft.Page):
                             ft.Row(
                                 [
                                     img_input, 
-                                    ft.ElevatedButton("Cargar imagen con fondo", on_click=cargar_imagen)
+                                    ft.ElevatedButton("Cargar imagen con fondo", on_click=cargar_imagen, tooltip="Selecciona una imagen para quitar el fondo")
                                 ]
                             ),
                             ft.Row(
                                 [
-                                    ft.ElevatedButton("Procesar carpeta de imágenes", on_click=seleccionar_carpeta_imagenes)
-                                ]
-                            ),
+                                    ft.ElevatedButton("Procesar carpeta de imágenes", on_click=seleccionar_carpeta_imagenes, tooltip="Selecciona una carpeta con imágenes para quitarles el fondo"),
+                                  ]
+                            )
                         ],
                         alignment=ft.MainAxisAlignment.START,
                         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                     ),
                     padding=container_padding,
                 ),
-                # Segunda columna
+                # Segunda columna con barra de progreso centrada y vista previa de imagen sin fondo
                 ft.Container(
                     content=ft.Column(
                         [
+                            
                             ft.Text("Vista previa de la imagen sin fondo:", weight=ft.FontWeight.BOLD),
                             removed_bg_image_preview,
-                            ft.ElevatedButton("Seleccionar carpeta destino", on_click=seleccionar_carpeta_destino),
-                            ft.ElevatedButton("Abrir carpeta destino", on_click=abrir_carpeta_destino_click),
+                            ft.Row(
+                                [
+                                    ft.ElevatedButton("Seleccionar carpeta destino", on_click=seleccionar_carpeta_destino, tooltip="Selecciona una carpeta de destino"),
+                                ]
+                            ),
+                            ft.Row(
+                                [
+                                    ft.ElevatedButton("Abrir carpeta destino", on_click=abrir_carpeta_destino_click, tooltip="Abrir la carpeta de destino seleccionada")
+                                ]
+                            ),
+                            ft.Column(
+                                [
+                                    progress_text,  # Texto de progreso (inicialmente oculto)
+                                    progress  # Barra de progreso (inicialmente oculta)
+                                ],
+                                alignment=ft.MainAxisAlignment.CENTER,
+                                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                            )
                         ],
-                        alignment=ft.MainAxisAlignment.START,
+                        alignment=ft.MainAxisAlignment.CENTER,
                         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                     ),
                     padding=container_padding,
-                ),
+                )
             ],
-            alignment=ft.MainAxisAlignment.SPACE_AROUND,
-            spacing=40,
-        )
+            alignment=ft.MainAxisAlignment.SPACE_EVENLY
+        ),
+        snack_bar,
     )
 
-# Ejecutar la aplicación de Flet
-ft.app(target=main)
+    # Crear el icono en la bandeja del sistema
+    create_tray_icon(page)
+    
+ft.app(target=main, view=ft.AppView.FLET_APP, assets_dir='assets')
---
+++ menu.py
@@ -0,0 +1,92 @@
+import flet as ft
+import os
+
+class Menu:
+    def __init__(self, page):
+        self.page = page
+        self.create_menu()
+        self.setup_close_confirmation()  # Manejar el cierre de la ventana principal
+
+    def create_menu(self):
+        # Crear un botón de menú en la app bar
+        menu = ft.PopupMenuButton(
+            items=[
+                ft.PopupMenuItem(text="Cómo funciona", on_click=self.open_help_window),
+                ft.PopupMenuItem(text="Cerrar aplicación", on_click=self.confirm_exit),
+            ]
+        )
+        
+        # Asignar el appbar con el menú
+        self.page.appbar = ft.AppBar(
+            title=ft.Text("Elimina el fondo de imágenes"),
+            center_title=True,
+            bgcolor="purple",
+            actions=[menu]
+        )
+
+    def confirm_exit(self, e=None):
+        # Diálogo para confirmar la salida de la aplicación
+        close_dialog = ft.AlertDialog(
+            modal=True,
+            title=ft.Text("¿Estás seguro de que quieres salir?"),
+            content=ft.Text("Cualquier progreso no guardado se perderá."),
+            actions=[
+                ft.TextButton("Sí", on_click=lambda e: self.page.window.destroy()),  # Cerrar la aplicación
+                ft.TextButton("No", on_click=lambda e: self.page.close(close_dialog))  # Cerrar el diálogo
+            ],
+            actions_alignment=ft.MainAxisAlignment.END,
+        )
+        self.page.overlay.append(close_dialog)  # Añadir el diálogo a la página
+        close_dialog.open = True  # Abrir el diálogo
+        self.page.update()
+
+    def open_help_window(self, e):
+        # Crear un AlertDialog para explicar el funcionamiento con tamaño controlado
+        help_dialog = ft.AlertDialog(
+            modal=True,
+            title=ft.Text("Cómo funciona el programa", color=ft.colors.WHITE),
+            content=ft.Container(
+                content=ft.Column(
+                    [
+                        ft.Text(
+                            "Este programa elimina el fondo de imágenes.",
+                            weight=ft.FontWeight.BOLD,
+                            color=ft.colors.RED
+                        ),
+                        ft.Text("1. Presiona 'Procesar carpeta de imágenes' o 'Cargar imagen con fondo'.",
+                                color=ft.colors.WHITE),
+                        ft.Text("2. Selecciona la carpeta donde se guardarán las imágenes sin fondo.",
+                                color=ft.colors.WHITE),
+                        ft.Text("3. El proceso tardará más o menos en función de la cantidad de imágenes a procesar.",
+                                color=ft.colors.WHITE),
+                        ft.Text("4. Las imágenes sin fondo se guardarán en la carpeta seleccionada. Todas ellas tendrán el mismo nombre \npero se añadirá '_sinfondo.png' al final.",
+                                color=ft.colors.WHITE),
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                    spacing=10
+                ),
+                padding=ft.padding.all(10),  # Padding interno
+                width=400,  # Ancho fijo para el diálogo
+                height=250,  # Altura controlada
+                alignment=ft.alignment.center  # Centrar contenido dentro del contenedor
+            ),
+            actions=[
+                ft.TextButton("Cerrar", on_click=lambda e: self.page.close(help_dialog))
+            ],
+            actions_alignment=ft.MainAxisAlignment.END,
+            bgcolor=ft.colors.BLACK,
+            inset_padding=ft.Padding(20, 20, 20, 20),  # Padding externo ajustado
+        )
+        self.page.overlay.append(help_dialog)
+        help_dialog.open = True  # Abrir el diálogo de ayuda
+        self.page.update()
+
+    def setup_close_confirmation(self):
+        # Manejar el evento de cierre de la ventana (cuando se pulsa la X)
+        def handle_window_event(e):
+            if e.data == "close":
+                self.confirm_exit()  # Abrir el diálogo de confirmación de salida
+
+        self.page.window.prevent_close = True  # Evitar el cierre directo de la ventana
+        self.page.window.on_event = handle_window_event  # Asignar el evento de cierre
